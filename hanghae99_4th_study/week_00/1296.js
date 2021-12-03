7ee931d0e7e2fca077ea40e7926d57daa72e620c
TnIoP <pointblank94@naver.com>
2021-12-03 23:27:46 +0900

    1296 clear

--- hanghae99_4th_study/week_00/1296.js
+++ hanghae99_4th_study/week_00/1296.js
@@ -1,38 +1,39 @@
 const fs = require("fs");
-const input = fs.readFileSync("ex.txt").toString().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const yeondu = input[0];
-const TESTCASE = Number(input[1]);
-input[0] = "";
-input[1] = "";
-input.sort();
+const yeondu = input.shift();
+const TESTCASE = Number(input.shift());
 
-
-let l = 0, o = 0, v = 0, e = 0, max = 0, index = 2;
+let l = 0, o = 0, v = 0, e = 0, max = 0;
 let tempY = [];
 let temp = [];
+let answer = [];
 
-for (let i = 2; i < TESTCASE+2; i++) {
+for (let i = 0; i < TESTCASE; i++) {
   tempY = yeondu.split("");
   for (let j = 0; j < tempY.length; j++) {
-    if (tempY[j] === "L") l++;
-    else if (tempY[j] === "O") o++;
-    else if (tempY[j] === "V") v++;
-    else if (tempY[j] === "E") e++;
+    if (tempY[j] == "L") l++;
+    else if (tempY[j] == "O") o++;
+    else if (tempY[j] == "V") v++;
+    else if (tempY[j] == "E") e++;
   }
 
   temp = input[i].split("");
-  console.log(temp)
   for (let j = 0; j < temp.length; j++) {
-    if (temp[j] === "L") l++;
-    else if (temp[j] === "O") o++;
-    else if (temp[j] === "V") v++;
-    else if (temp[j] === "E") e++;
+    if (temp[j] == "L") l++;
+    else if (temp[j] == "O") o++;
+    else if (temp[j] == "V") v++;
+    else if (temp[j] == "E") e++;
   }
 
-  if (max < ((l + o) * (l + v) * (l + e) * (o + v) * (o + e) * (v + e)) % 100) {
+  if (max <= ((l + o) * (l + v) * (l + e) * (o + v) * (o + e) * (v + e)) % 100) {
+    if(max === ((l + o) * (l + v) * (l + e) * (o + v) * (o + e) * (v + e)) % 100) {
+      answer.push(input[i])
+    } else {
+      answer = [];
+      answer.push(input[i])
+    }
     max = ((l + o) * (l + v) * (l + e) * (o + v) * (o + e) * (v + e)) % 100;
-    index = i;
   }
   l = 0;
   o = 0;
@@ -40,4 +41,4 @@ for (let i = 2; i < TESTCASE+2; i++) {
   e = 0;
 }
 
-console.log(input[index])
+console.log(answer.sort()[0]);
