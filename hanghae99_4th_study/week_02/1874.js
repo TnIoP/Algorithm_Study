c989b716ef435178465e40c9ae2aaddca67acc3b
TnIoP <pointblank94@naver.com>
2021-12-08 02:06:34 +0900

    알고리즘 2주차 문제 set

---
+++ hanghae99_4th_study/week_02/1874.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync("ex.txt").toString().split("\n");
+
+const TESTCASE = input[0];
+let temp = "";
+console.log(input)
+for(let i=1; i<=TESTCASE; i++) {
+    temp = input[i]
+    
+
+}
