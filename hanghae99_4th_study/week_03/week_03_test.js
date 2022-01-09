cebaca681003b2fb0d0c7d008051854dbb01d975
TnIoP <pointblank94@naver.com>
2022-01-09 17:59:18 +0900

    - 주석수정

--- hanghae99_4th_study/week_03/week_03_test.js
+++ hanghae99_4th_study/week_03/week_03_test.js
@@ -17,7 +17,7 @@ function solution(expression) {
         temp[j] = Number(temp[j]);
 
     for (let j = 0; j < operator[i].length; j++) { // 수식 3개만큼 반복
-      while (temp.includes(operator[i][j])) { // 정해놓은 수식의 우선순위 순서대로 수식을 수행
+      while (temp.includes(operator[i][j])) { // 정해놓은 수식의 우선순위 순서대로 수식에 포함된 모든 연산자를 수행.
         const index = temp.indexOf(operator[i][j]); // 일치하는 수식의 인덱스
         if (operator[i][j] === "+") // 덧셈
           temp[index - 1] += temp[index + 1];
@@ -25,7 +25,7 @@ function solution(expression) {
           temp[index - 1] -= temp[index + 1];
         else if (operator[i][j] === "*") // 곱셈
           temp[index - 1] *= temp[index + 1];
-          
+
         temp.splice(index, 2); // 해당 수식을 수행했으므로 수식과 수식 뒤에 숫자 삭제
       }
     }
