a6a8978cbc581d124dd1049043b79e4060adf6cf
TnIoP <pointblank94@naver.com>
2022-02-20 23:48:12 +0900

    - python prac 49 solved

---
+++ Programmers/python/prac/q49_체육복.py
@@ -0,0 +1,18 @@
+def solution(n, lost, reserve):
+    answer = 0
+    studentInfo = [1 for i in range(n)]
+    for i in range(len(lost)):
+        studentInfo[lost[i] - 1] -= 1
+    for i in range(len(reserve)):
+        studentInfo[reserve[i] - 1] += 1
+    for i in range(len(studentInfo)):
+        if studentInfo[i] == 0:
+            if studentInfo[i - 1] == 2:
+                studentInfo[i - 1] -= 1
+                studentInfo[i] += 1
+            elif studentInfo[i + 1] == 2:
+                studentInfo[i + 1] -= 1
+                studentInfo[i] += 1
+        if studentInfo[i] != 0:
+            answer += 1
+    return answer
