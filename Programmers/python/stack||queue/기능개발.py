88c4eb343919ec91e6c301a22e0c3dcf8a3bffc3
TnIoP <pointblank94@naver.com>
2022-02-26 23:50:18 +0900

    - programmers 기능개발 solved (modify)

--- Programmers/python/stack||queue/기능개발.py
+++ Programmers/python/stack||queue/기능개발.py
@@ -1,6 +1,5 @@
 import math
 
-
 def solution(progresses, speeds):
     answer = []
     date = [int(math.ceil(float(100-progresses[i])/float(speeds[i])))
@@ -14,17 +13,11 @@ def solution(progresses, speeds):
     for i in range(len(date)):
         if date[pointer] >= date[i]:
             if i != 0:
-                count+=1
+                count += 1
         else:
             answer.append(count)
             count = 1
             pointer = i
         if i == len(date)-1:
-            if date[len(date)-2] < date[len(date)-1]:
-                answer.append(1)
-            else:
-                answer.append(count)
+            answer.append(count)
     return answer
-
-
-print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
