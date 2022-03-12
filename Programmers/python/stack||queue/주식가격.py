a128b061808eb2171ecc65481e42cc67a5646181
TnIoP <pointblank94@naver.com>
2022-03-12 12:49:50 +0900

    - programmers 주식가격 solved (modify)

--- Programmers/python/stack||queue/주식가격.py
+++ Programmers/python/stack||queue/주식가격.py
@@ -1,3 +1,13 @@
 def solution(prices):
     answer = []
+    count = 0
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                count += 1
+            else:
+                count += 1
+                break
+        answer.append(count)
+        count = 0
     return answer
