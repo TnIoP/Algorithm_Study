380e8bcfd27645012145a88f2cc21df41497bef3
TnIoP <pointblank94@naver.com>
2022-03-13 23:59:02 +0900

    - programmers 다리를 지나는 트럭 solved (modify)

--- Programmers/python/stack||queue/다리를_지나는_트럭.py
+++ Programmers/python/stack||queue/다리를_지나는_트럭.py
@@ -1,3 +1,12 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
+    on_truck = [0] * bridge_length
+    while on_truck != [] :
+        answer += 1
+        on_truck.pop(0)
+        if truck_weights :
+            if sum(on_truck) + truck_weights[0] <= weight :
+                on_truck.append(truck_weights.pop(0))
+            else :
+                on_truck.append(0)
     return answer
