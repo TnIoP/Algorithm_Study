34441c7f1e97eec263927f51520ceb6133f5ed7a
TnIoP <pointblank94@naver.com>
2022-03-16 22:59:56 +0900

    - baekjoon 11053, 1003 solved (modify)

--- Baekjoon/python/dynamic_programming/1003.py
+++ Baekjoon/python/dynamic_programming/1003.py
@@ -1,3 +1,14 @@
 from sys import stdin
 
-n = int(stdin.readline())
+t = int(stdin.readline())
+
+for i in range(t):
+    n = int(stdin.readline())
+    count_0 = 1
+    count_1 = 0
+    temp = 0
+    for i in range(n):
+        temp = count_1
+        count_1 = count_1 + count_0
+        count_0 = temp
+    print(count_0, count_1)
