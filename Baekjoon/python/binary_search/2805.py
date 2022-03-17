8fdade3626b40e91d3490b3f56211e294a266f15
TnIoP <pointblank94@naver.com>
2022-03-17 23:48:44 +0900

    - baekjoon 2805 solved

---
+++ Baekjoon/python/binary_search/2805.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+tree = list(map(int, stdin.readline().split()))
+
+high = max(tree)
+low = 1
+
+while low <= high:
+    mid = (high+low) // 2
+    temp = 0
+    for t in tree:
+        if t > mid:
+            temp += t - mid
+    if temp < m:
+        high = mid - 1
+    elif temp > m:
+        low = mid + 1
+    else:
+        break
+print(high)
